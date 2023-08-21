371453795bb101df95d804b8caafcb09b28be83d
thor <thor@wolpert.ca>
2023-08-21 15:00:34 -0700

    initial commit

---
+++ src/btr_api/models/submission.py
@@ -0,0 +1,105 @@
+# Copyright © 2023 Province of British Columbia
+#
+# Licensed under the BSD 3 Clause License, (the "License");
+# you may not use this file except in compliance with the License.
+# The template for the license can be found here
+#    https://opensource.org/license/bsd-3-clause/
+#
+# Redistribution and use in source and binary forms,
+# with or without modification, are permitted provided that the
+# following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Sample submission class."""
+from __future__ import annotations
+
+from datetime import datetime
+
+from sqlalchemy import desc
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import backref
+from sql_versioning import Versioned
+
+from .db import db
+from ..common.enum import auto
+from ..common.enum import BaseEnum
+
+
+class SubmissionType(BaseEnum):
+    """Enum of the roles used across the domain."""
+
+    other = auto()
+    standard = auto()
+
+
+class Submission(Versioned, db.Model):
+    """Stores a submission of JSON data."""
+
+    __tablename__ = "submission"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column("state", db.Enum(SubmissionType), default=SubmissionType.other)
+    submitted_date = db.Column("submitted_date", db.DateTime(timezone=True), default=datetime.now())
+    payload = db.Column("payload", JSONB)
+
+    # Relationships
+    submitter_id = db.Column('submitter_id', db.Integer,
+                             db.ForeignKey('users.id'))
+
+    submitter = db.relationship('User',
+                                backref=backref('filing_submitter', uselist=False),
+                                foreign_keys=[submitter_id])
+
+    # @property
+    # def json(self):
+    #     """Return a json representation of this object."""
+    #     submission_dict = {
+    #          'id': self.id,
+    #          'type': self.type,
+    #          'submittedDate': self.submitted_date.isoformat(),
+    #          'payload': self.payload
+    #     }
+    #     if self.submitter_id:
+    #             submission_dict['submitter'] = self.submitter.username
+
+
+    def save(self):
+        """Save and commit immediately."""
+        db.session.add(self)
+        db.session.commit()
+
+    def save_to_session(self):
+        """Save toThe session, do not commit immediately."""
+        db.session.add(self)
+
+    @classmethod
+    def find_by_id(cls, id):
+        """Return the submission by id."""
+        return cls.query.filter_by(id=id).one_or_none()
+
+    @classmethod
+    def get_filtered_submissions(cls):
+        """Return the submissions."""
+        query = cls.query.order_by(desc(Submission.submitted_date))
+        return query.all()
---
+++ src/btr_api/resources/submission.py
@@ -0,0 +1,69 @@
+# Copyright © 2023 Province of British Columbia
+#
+# Licensed under the BSD 3 Clause License, (the "License");
+# you may not use this file except in compliance with the License.
+# The template for the license can be found here
+#    https://opensource.org/license/bsd-3-clause/
+#
+# Redistribution and use in source and binary forms,
+# with or without modification, are permitted provided that the
+# following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+from http import HTTPStatus
+
+from flask import Blueprint
+from flask import jsonify
+from flask import request
+
+from btr_api.models import Submission
+
+
+bp = Blueprint("submission", __name__)
+
+
+@bp.route("/", methods=("GET",))
+@bp.route("/<id>", methods=("GET",))
+def registers(id: int | None = None):
+    """Get the submissions, or sepcific submission by id."""
+    if id:
+        if submission := Submission.find_by_id(id):
+            return jsonify(type=submission.type, submission=submission.payload)
+        return {}, HTTPStatus.NOT_FOUND
+
+    submissions = Submission.get_filtered_submissions()
+
+    return jsonify(submissions)
+
+
+@bp.route("/", methods=("POST",))
+def create_register():
+    if json_input := request.get_json():
+        # normally do some validation here
+        submission = Submission()
+        submission.payload = json_input
+        submission.save()
+        return jsonify(id=submission.id), HTTPStatus.CREATED
+
+    return {}, HTTPStatus.BAD_REQUEST
