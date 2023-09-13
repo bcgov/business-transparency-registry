ab029d207a05833dc8ff4d3484983c88adbd5761
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-09-13 15:06:40 -0700

    initialize pnpm workspaces, nuxt3 apps.

---
+++ btr-web/btr-layouts/components/buttons/BcrosButtonPrimary.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex">
+    <slot>
+      <!-- todo: add icon -->
+      <button class="primary-button px-10 rounded-[4px]" @click="buttonAction">
+        <span class="flex px-28 pt-5 pb-10 text-white">{{ buttonText }}</span>
+      </button>
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  buttonAction: {
+    type: Function,
+    required: true
+  },
+  buttonText: {
+    type: String,
+    required: true
+  },
+  buttonIcon: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  primary: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+</script>
+
+<!--&lt;!&ndash;// todo: add margins etc ...&ndash;&gt;-->
+<!--<style scoped>-->
+<!--.primary-button {-->
+<!--  margin: 5px;-->
+<!--  background-color: #1669BB;-->
+<!--  font-family: "BC Sans Regular", serif;-->
+<!--  font-size: 14px;-->
+<!--  color: white;-->
+<!--}-->
+<!--</style>-->
---
+++ btr-web/btr-layouts/components/header/BcrosHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--  todo: fixme -->
+  <div id="bcros-main-header" class="flex bcros-background px-10 h-[70px] items-center">
+    <a class="float-left flex items-center justify-center" href="/">
+      <picture>
+        <source media="(max-width:600px)" srcset="@/assets/images/gov_bc_logo_vert.png">
+        <img src="@/assets/images/gov_bc_logo_horiz.png" alt="Government of British Columbia Logo">
+      </picture>
+      <h1 class="text-white text-xl my-2 ml-5 font-bold">BC Registries and Online Services</h1>
+    </a>
+    <div id="header-actions" class="float-right flex h-full">
+      <BcrosButtonPrimary button-text="Btn1" :button-action="()=>{}" />
+      <BcrosButtonPrimary button-text="Btn2" :button-action="()=>{}" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.bcros-background {
+  background-color: #003366;
+  border-bottom: 2px solid #fcba19;
+}
+
+</style>
