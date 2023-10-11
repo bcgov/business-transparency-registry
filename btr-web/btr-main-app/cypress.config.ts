f7b11aa530570cd61095aedb3fe9067be23bf9e0
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-10-11 14:30:15 -0700

    Fixing lint for btr-main-app.

--- btr-web/btr-main-app/cypress.config.ts
+++ btr-web/btr-main-app/cypress.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'cypress'
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   e2e: {
--- btr-web/btr-main-app/tests/nuxt/index.spec.ts
+++ btr-web/btr-main-app/tests/nuxt/index.spec.ts
@@ -1,9 +1,9 @@
 import { expect, describe, test } from 'vitest'
-import LandingPage from '~/pages/index.vue'
 import { mountSuspended } from 'vitest-environment-nuxt/utils'
 
-describe('Tests for app.vue', async () => {
+import LandingPage from '~/pages/index.vue'
 
+describe('Tests for app.vue', () => {
   test('header and footer initialized', async () => {
     const wrapper = await mountSuspended(LandingPage)
     expect(wrapper.find('#bcros-main-header')).toBeTruthy()
