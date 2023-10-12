0a26eca7143b9d4bd9a8f9047b8ae47afd73def1
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-10-12 09:38:40 -0700

    Fix typo. Add emit type.

--- btr-web/btr-common-components/components/bcros/EmailField.vue
+++ btr-web/btr-common-components/components/bcros/EmailField.vue
@@ -15,7 +15,7 @@
 
 <script setup lang="ts">
 
-defineEmits(['update:modelValue'])
+defineEmits<{(e: 'update:modelValue', value: string): void}>()
 defineProps({
   label: { type: [String], default: '' },
   id: { type: String, required: true },
