d7b9ff68965ae522ebaaba5f9a9acae2039d12cf
patrickpeinanw <144158754+patrickpeinanw@users.noreply.github.com>
2023-10-13 09:55:37 -0700

    Add footer using Nuxt UI and Tailwind (#14)
    * Add footer with Tailwind styling
    
    * update the footer component and add unit tests
    
    * remove responsive design and re-write all css to tailwind
    
    ---------
    
    Aauthored-by: Patrick Wang <root@NI502169.idir.BCGOV>

--- btr-web/btr-layouts/components/footer/BcrosFooter.vue
+++ btr-web/btr-layouts/components/footer/BcrosFooter.vue
@@ -1,11 +1,34 @@
 <template>
-  <!--  todo: implment me -->
-  <div id="bcros-main-footer">
-    <h1>This is footer</h1>
-  </div>
+  <footer id="bcros-main-footer" class="flex items-center h-[56px] border-t-2 border-yellow-400 bg-blue-900 text-sm">
+    <div class="container m-auto w-[1360px] max-w-full h-[30.5px]">
+      <nav class="flex justify-between">
+        <ul class="p-0 list-none">
+          <li v-for="link in links" :key="link.text" class="inline-block mr-2 pr-2 border-r last:mr-0 last:border-r-0">
+            <a class="block py-1 px-2 text-white hover:underline" :href="link.href" :target="link.newTab ? '_blank' : '_self'">
+              {{ link.text }}
+            </a>
+          </li>
+        </ul>
+        <UTooltip id="footer-tooltip" class="mr-3 mt-1" :text="`BTR UI v${appVersion}`">
+          <div class="icon-container" role="img" tabindex="0">
+            <UIcon class="text-2xl text-white" name="i-mdi-information-outline" />
+          </div>
+        </UTooltip>
+      </nav>
+    </div>
+  </footer>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+defineProps<{appVersion: string}>()
+
+const links = [
+  { text: 'Home', href: '/', newTab: false },
+  { text: 'Disclaimer', href: 'https://www2.gov.bc.ca/gov/content/home/disclaimer', newTab: true },
+  { text: 'Privacy', href: 'https://www2.gov.bc.ca/gov/content/home/privacy', newTab: true },
+  { text: 'Accessibility', href: 'https://www2.gov.bc.ca/gov/content/home/accessibility', newTab: true },
+  { text: 'Copyright', href: 'https://www2.gov.bc.ca/gov/content/home/copyright', newTab: true }
+]
 </script>
 
 <style scoped>
