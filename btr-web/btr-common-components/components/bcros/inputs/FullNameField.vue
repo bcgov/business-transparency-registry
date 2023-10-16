5c8ed4a4748e10000c9abf84b0767fb3e2711af7
patrickpeinanw <144158754+patrickpeinanw@users.noreply.github.com>
2023-10-16 13:19:39 -0700

    Add the FullNameField component (#19)
    * Add FullNameField component
    
    * add unit test for FullNameField component
    
    ---------
    
    Co-authored-by: Patrick Wang <root@NI502169.idir.BCGOV>

---
+++ btr-web/btr-common-components/components/bcros/inputs/FullNameField.vue
@@ -0,0 +1,25 @@
+<template>
+    <UFormGroup :label="label" name="fullName">
+      <UInput
+        :id="id"
+        type="text"
+        v-bind="$attrs"
+        :value="modelValue"
+        color="gray"
+        class="border-b-[2px] bg-gray-100"
+        variant="none"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </UFormGroup>
+  </template>
+  
+  <script setup lang="ts">
+  
+  defineEmits<{(e: 'update:modelValue', value: string): void}>()
+  defineProps({
+    label: { type: [String], default: '' },
+    id: { type: String, required: true },
+    modelValue: { type: String, default: '' }
+  })
+  </script>
+  
---
+++ btr-web/btr-common-components/tests/unit/components/bcros/inputs/FullNameField.spec.ts
@@ -0,0 +1,11 @@
+// @vitest-environment nuxt
+import { it, expect } from 'vitest'
+import { mountSuspended } from 'vitest-environment-nuxt/utils'
+
+import { BcrosInputsFullNameField } from '#components'
+
+it('can mount some component', async () => {
+  const component = await mountSuspended( BcrosInputsFullNameField, { props: { label: 'Test Full Name', id: 'testId' } })
+  expect(component.findComponent('#testId')).toBeTruthy()
+  expect(component.text()).toMatchInlineSnapshot('"Test Full Name"')
+})
