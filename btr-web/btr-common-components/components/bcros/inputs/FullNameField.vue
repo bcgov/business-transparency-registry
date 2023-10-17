74c95154951a302738968bcae5956c11948b8db5
Patrick Wang <root@NI502169.idir.BCGOV>
2023-10-17 10:20:00 -0700

    display normalized name in the input field

--- btr-web/btr-common-components/components/bcros/inputs/FullNameField.vue
+++ btr-web/btr-common-components/components/bcros/inputs/FullNameField.vue
@@ -9,16 +9,24 @@
       class="border-b-[2px] bg-gray-100"
       variant="none"
       @input="$emit('update:modelValue', $event.target.value)"
+      @blur="normalizeInput"
     />
   </UFormGroup>
 </template>
 
 <script setup lang="ts">
+import { normalizeName } from '~/utils/validation/form_inputs'
 
-defineEmits<{(e: 'update:modelValue', value: string): void}>()
-defineProps({
+const emit = defineEmits<{(e: 'update:modelValue', value: string): void}>()
+
+const props = defineProps({
   label: { type: [String], default: '' },
   id: { type: String, required: true },
   modelValue: { type: String, default: '' }
 })
+
+const normalizeInput = () => {
+  const normalizedValue = normalizeName(props.modelValue)
+  emit('update:modelValue', normalizedValue)
+}
 </script>
