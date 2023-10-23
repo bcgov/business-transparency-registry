f1865393272b03836cc440fb3ac9ddd577294abc
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-10-22 22:17:02 -0700

    Setting common styles for inputs (Nuxt UI). Moving lint files to global dir.

--- btr-web/btr-common-components/app.config.ts
+++ btr-web/btr-common-components/app.config.ts
@@ -3,7 +3,25 @@ export default defineAppConfig({
     primary: 'bcGovBlue',
     gray: 'bcGovGray',
     input: {
-      rounded: 'rounded-none'
+      rounded: 'rounded-none',
+      variant: {
+        bcGov: 'bg-gray-100 border-b-2 focus:ring-0'
+      }
+    },
+    select: {
+      rounded: 'rounded-none',
+      variant: {
+        bcGov: 'bg-gray-100 border-b-2 focus:ring-0'
+      }
+    },
+    selectMenu: {
+      rounded: 'rounded-none',
+      option: {
+        rounded: 'rounded-none'
+      },
+      variant: {
+        bcGov: 'bg-gray-100 border-b-2 focus:ring-0'
+      }
     }
   }
 })
