4c747de3241b1a895cce18d31725a8c32b6f2b91
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-10-24 10:32:52 -0700

    First version with working find address call.

---
+++ btr-web/btr-common-components/utils/debounce.ts
@@ -0,0 +1,16 @@
+export function debounce (func: Function, timeout = 250) {
+  let timer: string | number | NodeJS.Timeout | undefined;
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // @ts-ignore
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+// example
+// function saveInput(){
+//   console.log('Saving data');
+// }
+// const processChange = debounce(() => saveInput());
