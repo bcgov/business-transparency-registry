74b05673530c7665e0f7c3e891f4a757c1d4ee0e
Kial Jinnah <kialjinnah@gmail.com>
2023-11-10 14:35:11 -0800

    speed up dev build by ~50%
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- btr-web/btr-main-app/tailwind.config.ts
+++ btr-web/btr-main-app/tailwind.config.ts
@@ -1,5 +1,15 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
-  content: ['*.{html,ts,js,vue}']
+  content: [
+    './app/*.{html,ts,js,vue}',
+    './components/*.{html,ts,js,vue}',
+    './enums/*.{html,ts,js,vue}',
+    './interfaces/*.{html,ts,js,vue}',
+    './middleware/*.{html,ts,js,vue}',
+    './pages/*.{html,ts,js,vue}',
+    './stores/*.{html,ts,js,vue}',
+    './utils/*.{html,ts,js,vue}',
+    './app.vue'
+  ]
 }
