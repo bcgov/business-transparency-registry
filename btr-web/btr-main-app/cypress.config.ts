ee054e12e136df7b23e60309ba7f140dcb0b704a
Kial Jinnah <kialj876@gmail.com>
2024-01-23 08:59:13 -0800

    updated for lint
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- btr-web/btr-main-app/cypress.config.ts
+++ btr-web/btr-main-app/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents (on, config) {
+    setupNodeEvents (on) {
       on('task', {
         log (message) {
           console.info(message)
