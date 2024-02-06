8f68d8bb8de71d9735106eb5c85a13f3ffea6291
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-02-06 14:54:05 -0800

    Align radio top, display exteranl influence in any case. (#113)
    * Align radio top, display exteranl influence in any case.
    
    * Adding some extra spacing, as per ticket comments.

--- btr-web/btr-main-app/components/individual-person/AddNew.vue
+++ btr-web/btr-main-app/components/individual-person/AddNew.vue
@@ -213,34 +213,37 @@
         <p>
           <BcrosI18HelperBold translation-path="texts.externalInfluence.text" />
         </p>
-        <div class="flex items-center mb-2 py-1">
+        <div class="flex items-top mb-2 py-1">
           <URadio
-            id="radio-can-influence"
+            id="radio-no-influence"
             v-model="significantIndividual.externalInfluence"
             :value="ExternalInfluenceE.NO_EXTERNAL_INFLUENCE"
             data-cy="external-influence-radio-no-influence"
+            class="pt-1"
           />
-          <label for="radio-can-influence" class="ml-5">
+          <label for="radio-no-influence" class="ml-5">
             <BcrosI18HelperBold translation-path="labels.externalInfluence.noExternalInfluence" />
           </label>
         </div>
-        <div class="flex items-center mb-2 py-1">
+        <div class="flex items-top mb-2 py-1">
           <URadio
             id="radio-can-be-influenced"
             v-model="significantIndividual.externalInfluence"
             :value="ExternalInfluenceE.CAN_BE_INFLUENCED"
             data-cy="external-influence-radio-can-be-influenced"
+            class="pt-1"
           />
           <label for="radio-can-influence" class="ml-5">
             <BcrosI18HelperBold translation-path="labels.externalInfluence.canBeInfluenced" />
           </label>
         </div>
-        <div class="flex items-center mb-2 py-1">
+        <div class="flex items-top mb-2 py-1">
           <URadio
             id="radio-can-influence"
             v-model="significantIndividual.externalInfluence"
             :value="ExternalInfluenceE.CAN_INFLUENCE"
             data-cy="external-influence-radio-can-influence"
+            class="pt-1"
           />
           <label for="radio-can-influence" class="ml-5">
             <BcrosI18HelperBold translation-path="labels.externalInfluence.canInfluence" />
