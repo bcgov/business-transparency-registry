f78bb5a9cee41fdc729fabf9d75e06a95dbccde0
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-03-08 10:30:47 -0800

    Fix so page runs as expected after nuxt upgrade.

--- btr-web/btr-common-components/nuxt.config.ts
+++ btr-web/btr-common-components/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   ui: {
     icons: ['mdi'] // add here more icon sets from iconifiy if needed.
   },
