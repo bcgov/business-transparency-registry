962f6709d6a65872f5d40a73359e4c69015ad3eb
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-03-12 14:27:50 -0700

    Stubbing the intercepted calls.

--- btr-web/btr-layouts/cypress/e2e/breadcrumb.cy.ts
+++ btr-web/btr-layouts/cypress/e2e/breadcrumb.cy.ts
@@ -1,11 +1,11 @@
 describe('Layout -> Breadcrumb', () => {
   beforeEach(() => {
-    cy.intercept('https://app.launchdarkly.com/**').as('DARKLY')
-    cy.intercept('POST', 'https://events.launchdarkly.com/**').as('DARKLY_POST')
+    cy.intercept('https://app.launchdarkly.com/**', {}).as('DARKLY')
+    cy.intercept('POST', 'https://events.launchdarkly.com/**', {}).as('DARKLY_POST')
     cy.intercept('GET', 'https://dev.loginproxy.gov.bc.ca/**').as('LOGIN_PROXY')
     cy.visit('/')
     // give time for the keycloak init / page hydration
-    cy.wait(['@LOGIN_PROXY', '@DARKLY', '@DARKLY_POST'], { timeout: 15000 })
+    cy.wait(['@LOGIN_PROXY', '@DARKLY', '@DARKLY_POST'])
   })
 
   it('renders breadcrumbs', () => {
--- btr-web/btr-layouts/cypress/e2e/header.cy.ts
+++ btr-web/btr-layouts/cypress/e2e/header.cy.ts
@@ -7,13 +7,12 @@ describe('Layout -> Header (logged out)', () => {
   const headerMenus = headerContainerActions + '__menus'
 
   beforeEach(() => {
-    cy.intercept('https://app.launchdarkly.com/**').as('DARKLY')
-    cy.intercept('POST', 'https://events.launchdarkly.com/**').as('DARKLY_POST')
+    cy.intercept('https://app.launchdarkly.com/**', {}).as('DARKLY')
+    cy.intercept('POST', 'https://events.launchdarkly.com/**', {}).as('DARKLY_POST')
     cy.intercept('GET', 'https://dev.loginproxy.gov.bc.ca/**').as('LOGIN_PROXY')
     cy.visit('/')
     // give time for the keycloak init / page hydration
-    cy.wait(['@LOGIN_PROXY', '@DARKLY', '@DARKLY_POST'], { timeout: 15000 })
-    cy.wait(100)
+    cy.wait(['@LOGIN_PROXY', '@DARKLY', '@DARKLY_POST'])
   })
 
   it('renders header in logged out state', () => {
