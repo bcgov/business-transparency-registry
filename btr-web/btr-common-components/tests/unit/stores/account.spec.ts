2e6b17ec3062c9de5f0f1374e0fa10f0857bb726
patrickpeinanw <144158754+patrickpeinanw@users.noreply.github.com>
2024-04-05 16:42:32 -0700

    Fix failing unit tests after Vitest upgrade (#144)
    * fix unit tests
    
    * fix more tests and clean up

--- btr-web/btr-common-components/tests/unit/stores/account.spec.ts
+++ btr-web/btr-common-components/tests/unit/stores/account.spec.ts
@@ -1,74 +1,74 @@
 import { describe, expect, it, vi } from 'vitest'
-// import { setActivePinia, createPinia } from 'pinia'
-// import { axiosRequestMocks, axiosDefaultMock } from '../utils/mockedAxios'
-import { axiosDefaultMock } from '../utils/mockedAxios'
-// import { testParsedToken, testProfile, testUserSettings } from '../utils/mockedData'
-// import { useBcrosAccount } from '@/stores/account'
-// import { useBcrosKeycloak } from '@/stores/keycloak'
+import { setActivePinia, createPinia } from 'pinia'
+import { axiosRequestMocks, axiosDefaultMock } from '../utils/mockedAxios'
+import { testParsedToken, testProfile, testUserSettings } from '../utils/mockedData'
+import { useBcrosAccount } from '@/stores/account'
+import { useBcrosKeycloak } from '@/stores/keycloak'
 
 describe('Account Store Tests', () => {
-  // let account: any
-  // let keycloak: any
+  let account: any
+  let keycloak: any
+  let apiURL: string
+
   // axios mocks
   vi.mock('axios', () => { return { default: { ...axiosDefaultMock } } })
 
   beforeEach(() => {
-    // setActivePinia(createPinia())
-    // keycloak = useBcrosKeycloak()
+    setActivePinia(createPinia())
+    keycloak = useBcrosKeycloak()
     // account uses kcUser which is based off this
-    // keycloak.kc.tokenParsed = testParsedToken
+    keycloak.kc.tokenParsed = testParsedToken
 
-    // account = useBcrosAccount()
-    // // for some reason these don't initialize properly
-    // account.user = computed(() => keycloak.kcUser)
-    // account.userFirstName = ref(account.user.firstName)
-    // account.userLastName = ref(account.user.lastName)
+    account = useBcrosAccount()
+    // for some reason these don't initialize properly
+    account.user = computed(() => keycloak.kcUser)
+    account.userFirstName = ref(account.user.firstName)
+    account.userLastName = ref(account.user.lastName)
+
+    apiURL = useRuntimeConfig().public.authApiURL
   })
 
   afterEach(() => vi.clearAllMocks())
 
   it('renders default state/getters as expected', () => {
-    expect(false === true)
-    // expect(account.currentAccount).toEqual({})
-    // expect(account.currentAccountName).toBe('')
-    // expect(account.user).toEqual(keycloak.kcUser)
-    // expect(account.userFirstName).toBe(testParsedToken.firstname)
-    // expect(account.userLastName).toBe(testParsedToken.lastname)
-    // expect(account.errors).toEqual([])
+    expect(account.currentAccount).toEqual({})
+    expect(account.currentAccountName).toBe('')
+    expect(account.user).toEqual(keycloak.kcUser)
+    expect(account.userFirstName).toBe(testParsedToken.firstname)
+    expect(account.userLastName).toBe(testParsedToken.lastname)
+    expect(account.errors).toEqual([])
   })
 
-  // it('sets name values as expected when setUserName is called (non BCeId)', async () => {
-  //   expect(account.user.loginSource).not.toBe(LoginSourceE.BCEID)
-  //   await account.setUserName()
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   expect(account.userFirstName).toBe(testParsedToken.firstname)
-  //   expect(account.userLastName).toBe(testParsedToken.lastname)
-  // })
-  //
-  // it('sets name values as expected when setUserName is called (BCeId)', async () => {
-  //   keycloak.kc.tokenParsed.loginSource = LoginSourceE.BCEID
-  //   account.user.value = keycloak.kcUser
-  //   expect(account.user.loginSource).toBe(LoginSourceE.BCEID)
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   await account.setUserName()
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledWith(
-  //     'https://auth-api-dev.apps.silver.devops.gov.bc.ca/api/v1/users/@me')
-  //   expect(axiosRequestMocks.get).toHaveReturnedWith({ data: testProfile })
-  // })
-  //
-  // it('sets account values as expected when setAccountInfo is called', async () => {
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   expect(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT)).toBeNull()
-  //   await account.setAccountInfo()
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledWith(
-  //     `https://auth-api-dev.apps.silver.devops.gov.bc.ca/api/v1/users/${account.user.keycloakGuid}/settings`)
-  //   expect(account.currentAccount).toEqual(testUserSettings[0])
-  //   expect(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT)).toBe(JSON.stringify(testUserSettings[0]))
-  //   // test setting the current account to the 2nd value
-  //   sessionStorage.setItem(SessionStorageKeyE.CURRENT_ACCOUNT, JSON.stringify(testUserSettings[1]))
-  //   await account.setAccountInfo()
-  //   expect(account.currentAccount).toEqual(testUserSettings[1])
-  // })
+  it('sets name values as expected when setUserName is called (non BCeId)', async () => {
+    expect(account.user.loginSource).not.toBe(LoginSourceE.BCEID)
+    await account.setUserName()
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    expect(account.userFirstName).toBe(testParsedToken.firstname)
+    expect(account.userLastName).toBe(testParsedToken.lastname)
+  })
+
+  it('sets name values as expected when setUserName is called (BCeId)', async () => {
+    keycloak.kc.tokenParsed.loginSource = LoginSourceE.BCEID
+    account.user.value = keycloak.kcUser
+    expect(account.user.loginSource).toBe(LoginSourceE.BCEID)
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    await account.setUserName()
+    expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
+    expect(axiosRequestMocks.get).toHaveBeenCalledWith(`${apiURL}/users/@me`)
+    expect(axiosRequestMocks.get).toHaveReturnedWith({ data: testProfile })
+  })
+
+  it('sets account values as expected when setAccountInfo is called', async () => {
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    expect(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT)).toBeNull()
+    await account.setAccountInfo()
+    expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
+    expect(axiosRequestMocks.get).toHaveBeenCalledWith(`${apiURL}/users/${account.user.keycloakGuid}/settings`)
+    expect(account.currentAccount).toEqual(testUserSettings[0])
+    expect(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT)).toBe(JSON.stringify(testUserSettings[0]))
+    // test setting the current account to the 2nd value
+    sessionStorage.setItem(SessionStorageKeyE.CURRENT_ACCOUNT, JSON.stringify(testUserSettings[1]))
+    await account.setAccountInfo()
+    expect(account.currentAccount).toEqual(testUserSettings[1])
+  })
 })
--- btr-web/btr-common-components/tests/unit/stores/business.spec.ts
+++ btr-web/btr-common-components/tests/unit/stores/business.spec.ts
@@ -1,9 +1,7 @@
 import { describe, expect, it, vi } from 'vitest'
 import { setActivePinia, createPinia, storeToRefs } from 'pinia'
-// import { axiosRequestMocks, axiosDefaultMock } from '../utils/mockedAxios'
-import { axiosDefaultMock } from '../utils/mockedAxios'
-// import { testBusinessBEN, testBusinessSP, testBusinessContact } from '../utils/mockedData'
-import { testBusinessBEN, testBusinessSP } from '../utils/mockedData'
+import { axiosRequestMocks, axiosDefaultMock } from '../utils/mockedAxios'
+import { testBusinessBEN, testBusinessSP, testBusinessContact } from '../utils/mockedData'
 import { useBcrosBusiness } from '@/stores/business'
 
 describe('Business Store Tests', () => {
@@ -12,8 +10,8 @@ describe('Business Store Tests', () => {
   const {
     currentBusiness,
     currentBusinessIdentifier,
-    currentBusinessName
-    //, currentBusinessContact
+    currentBusinessName,
+    currentBusinessContact
   } = storeToRefs(business)
   // axios mocks
   vi.mock('axios', () => { return { default: { ...axiosDefaultMock } } })
@@ -41,72 +39,71 @@ describe('Business Store Tests', () => {
     expect(currentBusinessName.value).toBe(testBusinessSP.business.alternateNames[0].operatingName)
   })
 
-  // todo: re-evaluate if fix or remove tests below; ticket #20310
-  // it('retrieves business data when getBusinessDetails is called', async () => {
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   const businessDetails = await business.getBusinessDetails(testBusinessBEN.business.identifier, { slim: true })
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledWith(
-  //     `https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/${testBusinessBEN.business.identifier}`,
-  //     { params: { slim: true } }
-  //   )
-  //   expect(axiosRequestMocks.get).toHaveReturnedWith({ data: testBusinessBEN })
-  //   expect(businessDetails).toBeDefined()
-  //   if (businessDetails) {
-  //     // if condition required for typing due to possible void response
-  //     expect(businessDetails.identifier).toBe(testBusinessBEN.business.identifier)
-  //     expect(businessDetails.legalName).toBe(testBusinessBEN.business.legalName)
-  //   }
-  // })
-  //
-  // it('loads corresponding business data into the store when load business is called', async () => {
-  //   await business.loadBusiness(testBusinessBEN.business.identifier)
-  //   expect(currentBusiness.value).toEqual(testBusinessBEN.business)
-  //   expect(currentBusinessIdentifier.value).toEqual(testBusinessBEN.business.identifier)
-  //   expect(currentBusinessName.value).toEqual(testBusinessBEN.business.legalName)
-  // })
-  //
-  // it('does not load corresponding business data if it is already cached when load business is called', async () => {
-  //   currentBusiness.value = testBusinessBEN.business
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   await business.loadBusiness(testBusinessBEN.business.identifier)
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  // })
-  //
-  // it('loads corresponding business data if a different business is cached when load business is called',
-  // async () => {
-  //   currentBusiness.value = testBusinessSP
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   await business.loadBusiness(testBusinessBEN.business.identifier)
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
-  //   expect(currentBusiness.value).toEqual(testBusinessBEN.business)
-  // })
-  //
-  // it('loads corresponding business data if business is cached when load business is called with force', async () => {
-  //   currentBusiness.value = testBusinessBEN
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   await business.loadBusiness(testBusinessBEN.business.identifier, true)
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
-  // })
-  //
-  // it('loads corresponding contact data into the store with expected caching rules', async () => {
-  //   const expectedContact: ContactBusinessI = {
-  //     businessIdentifier: testBusinessContact.businessIdentifier,
-  //     ...testBusinessContact.contacts[0]
-  //   }
-  //   expect(axiosRequestMocks.get).not.toHaveBeenCalled()
-  //   await business.loadBusinessContact(testBusinessContact.businessIdentifier)
-  //   // should call the mock once and set the data
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
-  //   expect(currentBusinessContact.value).toEqual(expectedContact)
-  //   // calling again with the same identifier should not trigger the mocked call since it is already loaded
-  //   await business.loadBusinessContact(testBusinessContact.businessIdentifier)
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledTimes(1)
-  //   // calling again with force should trigger the mocked call again
-  //   await business.loadBusinessContact(testBusinessContact.businessIdentifier, true)
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledTimes(2)
-  //   // calling with a different identifier should trigger the mocked call
-  //   await business.loadBusinessContact(testBusinessContact.businessIdentifier + '1')
-  //   expect(axiosRequestMocks.get).toHaveBeenCalledTimes(3)
-  // })
+  it('retrieves business data when getBusinessDetails is called', async () => {
+    const apiURL = useRuntimeConfig().public.legalApiURL
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    const businessDetails = await business.getBusinessDetails(testBusinessBEN.business.identifier, { slim: true })
+    expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
+    expect(axiosRequestMocks.get).toHaveBeenCalledWith(
+      `${apiURL}/businesses/${testBusinessBEN.business.identifier}`,
+      { params: { slim: true } }
+    )
+    expect(axiosRequestMocks.get).toHaveReturnedWith({ data: testBusinessBEN })
+    expect(businessDetails).toBeDefined()
+    if (businessDetails) {
+      // if condition required for typing due to possible void response
+      expect(businessDetails.identifier).toBe(testBusinessBEN.business.identifier)
+      expect(businessDetails.legalName).toBe(testBusinessBEN.business.legalName)
+    }
+  })
+
+  it('loads corresponding business data into the store when load business is called', async () => {
+    await business.loadBusiness(testBusinessBEN.business.identifier)
+    expect(currentBusiness.value).toEqual(testBusinessBEN.business)
+    expect(currentBusinessIdentifier.value).toEqual(testBusinessBEN.business.identifier)
+    expect(currentBusinessName.value).toEqual(testBusinessBEN.business.legalName)
+  })
+
+  it('does not load corresponding business data if it is already cached when load business is called', async () => {
+    currentBusiness.value = testBusinessBEN.business
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    await business.loadBusiness(testBusinessBEN.business.identifier)
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+  })
+
+  it('loads corresponding business data if a different business is cached when load business is called', async () => {
+    currentBusiness.value = testBusinessSP
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    await business.loadBusiness(testBusinessBEN.business.identifier)
+    expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
+    expect(currentBusiness.value).toEqual(testBusinessBEN.business)
+  })
+
+  it('loads corresponding business data if business is cached when load business is called with force', async () => {
+    currentBusiness.value = testBusinessBEN
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    await business.loadBusiness(testBusinessBEN.business.identifier, true)
+    expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
+  })
+
+  it('loads corresponding contact data into the store with expected caching rules', async () => {
+    const expectedContact: ContactBusinessI = {
+      businessIdentifier: testBusinessContact.businessIdentifier,
+      ...testBusinessContact.contacts[0]
+    }
+    expect(axiosRequestMocks.get).not.toHaveBeenCalled()
+    await business.loadBusinessContact(testBusinessContact.businessIdentifier)
+    // should call the mock once and set the data
+    expect(axiosRequestMocks.get).toHaveBeenCalledOnce()
+    expect(currentBusinessContact.value).toEqual(expectedContact)
+    // calling again with the same identifier should not trigger the mocked call since it is already loaded
+    await business.loadBusinessContact(testBusinessContact.businessIdentifier)
+    expect(axiosRequestMocks.get).toHaveBeenCalledTimes(1)
+    // calling again with force should trigger the mocked call again
+    await business.loadBusinessContact(testBusinessContact.businessIdentifier, true)
+    expect(axiosRequestMocks.get).toHaveBeenCalledTimes(2)
+    // calling with a different identifier should trigger the mocked call
+    await business.loadBusinessContact(testBusinessContact.businessIdentifier + '1')
+    expect(axiosRequestMocks.get).toHaveBeenCalledTimes(3)
+  })
 })
