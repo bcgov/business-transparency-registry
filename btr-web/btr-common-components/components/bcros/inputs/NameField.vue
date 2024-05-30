8ac793297b9c06f8fb1d87eec8c696ecbd507e96
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-05-30 14:18:35 -0700

    21137 - fixed fullname error not being cleared when external change happens.

--- btr-web/btr-common-components/components/bcros/inputs/NameField.vue
+++ btr-web/btr-common-components/components/bcros/inputs/NameField.vue
@@ -4,7 +4,7 @@
       :id="id"
       type="text"
       v-bind="$attrs"
-      :value="modelValue"
+      :value="model"
       :variant="error ? 'error' : variant"
       :placeholder="placeholder"
       :disabled="isDisabled"
@@ -16,22 +16,32 @@
 
 <script setup lang="ts">
 import { normalizeName } from '~/utils/validation/form_inputs'
+import { type UseEventBusReturn } from '@vueuse/core'
 
-const emit = defineEmits<(e: 'update:modelValue', value: string) => void>()
+const formBus = inject<UseEventBusReturn<any, string> | undefined>('form-events', undefined)
+
+const model = defineModel({ type: String, default: '' })
+const emit = defineEmits()
 
 const props = defineProps({
   label: { type: String, default: '' },
   placeholder: { type: String, default: '' },
   id: { type: String, required: true },
-  name: { type: String, default: 'name' },
-  modelValue: { type: String, default: '' },
+  name: { type: String, default: 'name.fullName' },
   variant: { type: String, default: 'bcGov' },
   help: { type: String, default: '' },
   isDisabled: { type: Boolean, default: false }
 })
 
+watch(model, () => {
+  if (formBus) {
+    formBus.emit({ type: 'blur', path: props.name })
+    formBus.emit({ type: 'change', path: props.name })
+  }
+}, { deep: true })
+
 const normalizeInput = () => {
-  const normalizedValue = normalizeName(props.modelValue)
+  const normalizedValue = normalizeName(model.value)
   emit('update:modelValue', normalizedValue)
 }
 </script>
