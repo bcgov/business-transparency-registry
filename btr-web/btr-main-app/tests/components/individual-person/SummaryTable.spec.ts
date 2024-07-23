b64ddf4f5ee8594a630a3eae33cfd706bfa0fab3
Peinan Wang <peinanwang@RiddleMBP.local>
2024-07-23 14:05:48 -0700

    update unit test

--- btr-web/btr-main-app/tests/components/individual-person/SummaryTable.spec.ts
+++ btr-web/btr-main-app/tests/components/individual-person/SummaryTable.spec.ts
@@ -23,31 +23,26 @@ describe('AddIndividualPersonSummaryTable tests', () => {
   it('renders SummaryTable with no data provided', () => {
     const summaryTable = wrapper.find('[data-cy=individualsSummaryTable]')
     expect(summaryTable.exists()).toBe(true)
-    expect(summaryTable.findAll('th').length).toBe(5)
+    expect(summaryTable.findAll('th').length).toBe(4)
     expect(summaryTable.findAll('th').at(0)?.text()).toBe('Name')
-    expect(summaryTable.findAll('th').at(1)?.text()).toBe('Address')
-    expect(summaryTable.findAll('th').at(2)?.text()).toBe('Details')
-    expect(summaryTable.findAll('th').at(3)?.text()).toBe('Significance Dates')
-    expect(summaryTable.findAll('th').at(4)?.text()).toBe('Control')
+    expect(summaryTable.findAll('th').at(1)?.text()).toBe('Details')
+    expect(summaryTable.findAll('th').at(2)?.text()).toBe('Control')
+    expect(summaryTable.findAll('th').at(3)?.text()).toBe('Effective Dates')
     expect(summaryTable.findAll('td').length).toBe(1)
     expect(summaryTable.find('td').text()).toBe('You have no Significant individuals listed')
     expect(wrapper.find('[data-cy=summary-table-name]').exists()).toBe(false)
-    expect(wrapper.find('[data-cy=summary-table-address]').exists()).toBe(false)
     expect(wrapper.find('[data-cy=summary-table-details]').exists()).toBe(false)
+    expect(wrapper.find('[data-cy=summary-table-controls]').exists()).toBe(false)
     expect(wrapper.find('[data-cy=summary-table-dates]').exists()).toBe(false)
-    // todo: fixme: update on #TBD with new summary table
-    // expect(wrapper.find('[data-cy=summary-table-controls]').exists()).toBe(false)
     expect(wrapper.find('[data-cy=summary-table-buttons]').exists()).toBe(false)
   })
   it('updates SummaryTable with data dynamically', async () => {
     await wrapper.setProps({ individuals: [si] })
-    expect(wrapper.find('[data-cy=individualsSummaryTable] tbody tr').findAll('td').length).toBe(6)
+    expect(wrapper.find('[data-cy=individualsSummaryTable] tbody tr').findAll('td').length).toBe(5)
     expect(wrapper.find('[data-cy=summary-table-name]').exists()).toBe(true)
-    expect(wrapper.find('[data-cy=summary-table-address]').exists()).toBe(true)
     expect(wrapper.find('[data-cy=summary-table-details]').exists()).toBe(true)
+    expect(wrapper.find('[data-cy=summary-table-controls]').exists()).toBe(true)
     expect(wrapper.find('[data-cy=summary-table-dates]').exists()).toBe(true)
-    // todo: fixme: update on #TBD with new summary table
-    // expect(wrapper.find('[data-cy=summary-table-controls]').exists()).toBe(true)
     expect(wrapper.find('[data-cy=summary-table-buttons]').exists()).toBe(true)
   })
   it('displays the Name column as expected', async () => {
@@ -57,27 +52,21 @@ describe('AddIndividualPersonSummaryTable tests', () => {
     await wrapper.setProps({ individuals: [si] })
     expect(wrapper.find('[data-cy=summary-table-name]').text()).toContain(si.name.fullName.toUpperCase())
     expect(wrapper.find('[data-cy=summary-table-name]').text()).toContain(si.name.preferredName)
-    expect(wrapper.find('[data-cy=summary-table-name]').text()).toContain(si.email)
-  })
-  it('displays the Address column as expected', async () => {
-    await wrapper.setProps({ individuals: [si] })
-    expect(wrapper.find('[data-cy=summary-table-address]').text()).toContain(si.address.line1)
-    expect(wrapper.find('[data-cy=summary-table-address]').text()).toContain(si.address.city)
-    expect(wrapper.find('[data-cy=summary-table-address]').text()).toContain(si.address.region)
-    expect(wrapper.find('[data-cy=summary-table-address]').text()).toContain(si.address.postalCode)
-    expect(wrapper.find('[data-cy=summary-table-address]').text()).toContain(si.address.country.name)
+    expect(wrapper.find('[data-cy=summary-table-name]').text()).toContain(si.birthDate)
   })
   it('displays the Details column as expected', async () => {
     await wrapper.setProps({ individuals: [si] })
     const details = wrapper.find('[data-cy=summary-table-details]')
-    expect(details.text()).toContain(si.birthDate)
-    expect(details.text()).toContain(si.tax.taxNumber)
-    expect(details.text()).toContain('Citizenship(s):Canada')
-    expect(details.text()).toContain('Tax Resident of Canada')
+    expect(details.text()).toContain(si.email)
+    expect(details.text()).toContain(si.address.line1)
+    expect(details.text()).toContain(si.address.city)
+    expect(details.text()).toContain(si.address.region)
+    expect(details.text()).toContain(si.address.postalCode)
+    expect(details.text()).toContain('Tax Residency')
+    expect(details.text()).toContain('Canada')
   })
-  // todo: fixme: update on #TBD with new summary table && 20756
-  it.skip('displays the Controls column as expected', async () => {
-    // only shares
+  // To-Do: update this test to reflect the content of control column in the new summary table in #21234
+  it('displays the Controls column as expected', async () => {
     si.controlOfVotes.registeredOwner = true
     si.controlOfVotes.beneficialOwner = true
     si.controlOfVotes.indirectControl = true
@@ -94,71 +83,13 @@ describe('AddIndividualPersonSummaryTable tests', () => {
     si.controlOfDirectors.inConcertControl = false
     si.controlOther = ''
     await wrapper.setProps({ individuals: [si] })
-    // todo: fixme: update on #TBD with new summary table
-    // let controls = wrapper.find('[data-cy=summary-table-controls]')
-    // expect(controls.text()).toContain('Shares')
-    // expect(controls.text()).toContain('Registered owner, Beneficial owner, and Indirect control')
-    // expect(controls.text()).toContain('At least 25% and up to 50% of shares')
-    // expect(controls.text()).toContain('More than 50% and up to 75% of votes')
-    // expect(controls.text()).toContain('25% or more of shares or votes exercised in concert')
-    // expect(controls.text()).not.toContain('Directors')
-    // expect(controls.text()).not.toContain('Other')
-    // Only directors
-    // todo: fixme: update on #20756
-    // si.controlType.sharesVotes.registeredOwner = false
-    // si.controlType.sharesVotes.beneficialOwner = false
-    // si.controlType.sharesVotes.indirectControl = false
-    // si.controlType.sharesVotes.inConcertControl = false
-    // si.controlType.directors.directControl = true
-    // si.controlType.directors.indirectControl = true
-    // si.controlType.directors.significantInfluence = true
-    // si.controlType.directors.inConcertControl = true
-    // si.controlType.other = ''
-    await wrapper.setProps({ individuals: [si] })
-    controls = wrapper.find('[data-cy=summary-table-controls]')
-    expect(controls.text()).toContain('Directors')
-    expect(controls.text())
-      .toContain('Direct control, Indirect control, and Significant influence control of the majority of directors')
-    expect(controls.text()).toContain('Control of directors exercised in concert')
-    expect(controls.text()).not.toContain('Shares')
-    expect(controls.text()).not.toContain('Other')
-    // Only other control
-    si.controlType.sharesVotes.registeredOwner = false
-    si.controlType.sharesVotes.beneficialOwner = false
-    si.controlType.sharesVotes.indirectControl = false
-    si.controlType.sharesVotes.inConcertControl = false
-    si.controlType.directors.directControl = false
-    si.controlType.directors.indirectControl = false
-    si.controlType.directors.significantInfluence = false
-    si.controlType.directors.inConcertControl = false
-    si.controlType.other = 'My lawyer said so'
-    await wrapper.setProps({ individuals: [si] })
-    controls = wrapper.find('[data-cy=summary-table-controls]')
-    expect(controls.text()).toContain('Other')
-    expect(controls.text()).toContain(si.controlType.other)
-    expect(controls.text()).not.toContain('Shares')
+    const controls = wrapper.find('[data-cy=summary-table-controls]')
+    expect(controls.text()).toContain('Shares')
+    expect(controls.text()).toContain('Votes')
+    expect(controls.text()).toContain('Registered owner, Beneficial owner, and Indirect control')
+    expect(controls.text()).toContain('At least 25% and up to 50% of votes')
+    expect(controls.text()).toContain('More than 50% and up to 75% of shares')
     expect(controls.text()).not.toContain('Directors')
-    // all at once
-    si.controlType.sharesVotes.registeredOwner = true
-    si.controlType.sharesVotes.beneficialOwner = false
-    si.controlType.sharesVotes.indirectControl = true
-    si.controlType.sharesVotes.inConcertControl = true
-    si.controlType.directors.directControl = true
-    si.controlType.directors.indirectControl = true
-    si.controlType.directors.significantInfluence = false
-    si.controlType.directors.inConcertControl = false
-    si.controlType.other = 'My lawyer said so'
-    await wrapper.setProps({ individuals: [si] })
-    controls = wrapper.find('[data-cy=summary-table-controls]')
-    expect(controls.text()).toContain('Shares')
-    expect(controls.text()).toContain('Shares')
-    expect(controls.text()).toContain('Registered owner and Indirect control')
-    expect(controls.text()).toContain('25% or more of shares or votes exercised in concert')
-    expect(controls.text()).toContain('Directors')
-    expect(controls.text())
-      .toContain('Direct control and Indirect control of the majority of directors')
-    expect(controls.text()).not.toContain('Control of directors exercised in concert')
-    expect(controls.text()).toContain('Other')
-    expect(controls.text()).toContain(si.controlType.other)
+    expect(controls.text()).not.toContain('Other')
   })
 })
