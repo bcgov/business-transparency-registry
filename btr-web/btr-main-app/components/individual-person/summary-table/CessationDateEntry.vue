91b99782772d7031c01a76e98bc036800d14fd9f
Kial Jinnah <kialj876@gmail.com>
2024-08-16 12:12:18 -0700

    set min date on cessation date date picker
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- btr-web/btr-main-app/components/individual-person/summary-table/CessationDateEntry.vue
+++ btr-web/btr-main-app/components/individual-person/summary-table/CessationDateEntry.vue
@@ -3,10 +3,21 @@ import { SiSchemaType } from '~/utils/si-schema/definitions'
 
 const props = defineProps<{ individual: SiSchemaType, index: number }>()
 
-const { allEditableSIs }: {
-  allEditableSIs: Ref<SiSchemaType>
-} = storeToRefs(useSignificantIndividuals())
+const { allEditableSIs } = storeToRefs(useSignificantIndividuals())
 
+const currentDateGrp: Ref<{ startDate?: string, endDate?: string }> = ref({})
+const currentDateGrps = ref(props.individual.effectiveDates.filter(dateGrp => !dateGrp.endDate))
+onMounted(() => {
+  if (currentDateGrps.value.length > 0) {
+    if (currentDateGrps.value.length > 1) {
+      // should never get here
+      console.error('More than one current date group for individual')
+    }
+    currentDateGrp.value = currentDateGrps.value[0]
+  }
+})
+
+props.individual.effectiveDates.filter(dateGrp => !dateGrp.endDate)
 const cessationDate: Ref<string | undefined> = ref(undefined)
 const submitDate = () => {
   if (!cessationDate.value) {
@@ -29,8 +40,11 @@ const cancel = () => {
     :section-title="$t('labels.cessationDate')"
   >
     <div class="flex-col w-full">
+      <!-- NOTE: adding min date time so that it shows correctly for pacific time
+       (otherwise since startDate has no time / tz info so it defaults to 12am GMT in the date picker) -->
       <BcrosInputsDateSelect
         name="cessationDate"
+        :min-date="currentDateGrp.startDate ? currentDateGrp.startDate + 'T08:00:00' : undefined"
         :max-date="new Date()"
         :placeholder="$t('labels.endDate')"
         @selection="cessationDate = dateToString($event, 'YYYY-MM-DD')"
