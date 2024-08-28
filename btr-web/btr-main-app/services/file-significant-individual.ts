82558f41ee7059b02fd3ed66dc8611d6b2bb18dc
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-08-28 14:47:37 -0700

    allow information to be unspecified if reason is provided and box is … (#192)
    * allow information to be unspecified if reason is provided and box is ticked
    
    * address feedback

--- btr-web/btr-main-app/services/file-significant-individual.ts
+++ btr-web/btr-main-app/services/file-significant-individual.ts
@@ -117,8 +117,19 @@ const getPersonAndOwnershipAndControlStatements = (sif: SignificantIndividualFil
 
 const convertToBtrBodsForSubmit = (sif: SignificantIndividualFilingI): BtrFilingI => {
   const businessDetails = getCurrentBusinessAsBtrBodsEntityI()
-
   const { ownershipOrControlStatements, personStatements } = getPersonAndOwnershipAndControlStatements(sif)
+
+  // note that the api will receive empty strings and then fail validation
+  // as empty string is not null and not allowed on those fields.
+  // therefore this removes the empty strings to avoid that problem
+  for (const personS of personStatements) {
+    const k = Object.keys(personS)
+    for (let i = 0; i < k.length; i++) {
+      if ((typeof personS[k[i]] === 'string') && (personS[k[i]] === '')) {
+        delete personS[k[i]]
+      }
+    }
+  }
 
   return {
     noSignificantIndividualsExist: sif.noSignificantIndividualsExist,
