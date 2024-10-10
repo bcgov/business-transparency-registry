fe7c443b9e249a78e11fdef4b480f5185e473c9d
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-10-10 15:06:52 -0700

    fix unit tests

--- btr-web/btr-common-components/composables/useFetchBcros.ts
+++ btr-web/btr-common-components/composables/useFetchBcros.ts
@@ -2,9 +2,11 @@ export const useFetchBcros = <T>(request, opts?) => {
   if (!opts?.headers?.Authorization) {
     const token = useBcrosKeycloak().kc.token
 
-    const headers = opts.headers
-
     opts = opts || {}
+    let headers = {}
+    if (opts.headers) {
+      headers = opts.headers
+    }
     Object.assign(opts, { headers: { ...headers, Authorization: `Bearer ${token}` } })
   }
   if (!opts.headers['Account-Id']) {
