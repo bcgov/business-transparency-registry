b0e8ff1951def34e98738473bbb6ebb4a2acdcdc
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-01-31 10:12:15 -0800

    fix: corrections after merge

--- btr-web/btr-main-app/components/individual-person/add-new/index.vue
+++ btr-web/btr-main-app/components/individual-person/add-new/index.vue
@@ -564,7 +564,7 @@ const usePreferredNameCheckboxClickHandler = () => {
 
 const isNameChanging = ref(false)
 const isShowReasonForChange = computed(() => {
-  return props.editMode && isNameChanging.value && !inputFormSi.ui?.actions?.includes(FilingActionE.EDIT)
+  return props.editMode && isNameChanging.value && !inputFormSi.ui?.actions?.includes(FilingActionE.ADD)
 })
 
 const cancelNameChange = () => {
