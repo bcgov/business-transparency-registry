29baf1d8bf1d2d46c6b7a93e4b6fdb78b7eb5d3b
Peinan Wang <peinanwang@RiddleMBP.local>
2025-02-11 11:30:54 -0800

    PR update

--- btr-web/btr-main-app/components/individual-person/citizenship/index.vue
+++ btr-web/btr-main-app/components/individual-person/citizenship/index.vue
@@ -45,9 +45,7 @@ const updateCitizenship = () => {
   formBus?.emit({ type: 'change', path: props.name })
 }
 
-watch(citizenshipSelector, () => {
-  updateCitizenship()
-})
+watch(citizenshipSelector, updateCitizenship)
 </script>
 
 <template>
