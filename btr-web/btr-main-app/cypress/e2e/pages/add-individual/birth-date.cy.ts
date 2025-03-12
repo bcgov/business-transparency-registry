61b460b90e96e679a3181d616183f89df56c3e2c
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-03-12 10:38:24 -0700

    23119 - adding test

---
+++ btr-web/btr-main-app/cypress/e2e/pages/add-individual/birth-date.cy.ts
@@ -0,0 +1,20 @@
+import { dateToString } from '../../../../../btr-common-components/utils/date'
+
+describe('pages -> Add individual', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('verify birth date minor warning is there, and goes away if 19+ age is selected', () => {
+    cy.get('[data-cy=add-new-btn]').trigger('click')
+
+    cy.get('input[name="birthDate"][data-cy="date-select"]').type('1999-01-01')
+    cy.get('[data-cy="form-minor-warning"]').should('not.exist')
+
+    const today = new Date()
+    const todayString = dateToString(today, 'YYYY-MM-DD')
+
+    cy.get('input[name="birthDate"][data-cy="date-select"]').clear().type(todayString)
+    cy.get('[data-cy="form-minor-warning"]').should('exist')
+  })
+})
