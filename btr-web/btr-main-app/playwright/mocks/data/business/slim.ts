b758c12cb56352684c699e1020c734d6b9ab2755
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-05-26 11:51:16 -0700

    feat: adding playwright tests.

---
+++ btr-web/btr-main-app/playwright/mocks/data/business/slim.ts
@@ -0,0 +1,9 @@
+export const businessSlim = {
+  adminFreeze: false,
+  goodStanding: true,
+  identifier: 'BC0871427',
+  legalName: '0871427 B.C. LTD.',
+  legalType: 'BEN',
+  state: 'ACTIVE',
+  taxId: '123456789BC0001'
+}
